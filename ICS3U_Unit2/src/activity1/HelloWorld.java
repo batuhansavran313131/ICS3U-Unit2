3c0124f97c48ba8c82fd6f9a5dbeb764cb16f5ea
batuh <batuh@10.78.26.31>
2026-09-22 09:25:48 -0400

    Unit 1 Activity 1

--- ICS3U_Unit2/src/activity1/HelloWorld.java
+++ ICS3U_Unit2/src/activity1/HelloWorld.java
@@ -1,7 +1,12 @@
 package activity1;
 
+import simpleIO.Console;
+
 public class HelloWorld {
+	
     public static void main(String[] args) {
-        System.out.println("Hello World");
+    	
+        Console.print("Hello World");
+        
     }
 }
