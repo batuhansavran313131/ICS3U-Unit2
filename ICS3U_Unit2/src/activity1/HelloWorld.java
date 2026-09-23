8089b96be32c0cd8e4060d46c80ebd7a71755680
batuh <batuh@10.78.26.31>
2026-09-23 11:02:43 -0400

    I finished activity 1 Hello World.

--- ICS3U_Unit2/src/activity1/HelloWorld.java
+++ ICS3U_Unit2/src/activity1/HelloWorld.java
@@ -5,8 +5,8 @@ import simpleIO.Console;
 public class HelloWorld {
 	
     public static void main(String[] args) {
-    	
+    	//
         Console.print("Hello World");
-        
+       
     }
 }
